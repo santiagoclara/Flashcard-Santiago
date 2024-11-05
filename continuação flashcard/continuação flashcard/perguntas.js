c7c4fe35c75defaae64b5232f518c05447760283
Maria Clara <132940309+santiagoclara@users.noreply.github.com>
2024-11-05 09:57:27 -0300

    Add files via upload

---
+++ continuação flashcard/continuação flashcard/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+'Mundo Pop',
+'Quem é a melhor cantora do mundo?',
+'Beyoncé Giselle Knowles-Carter'
+)
+criaCartao(
+    'Beyoncé',
+    'Qual seu melhor álbum?',
+    'O álbum auto-denominado, Beyoncé'
+    )
+ criaCartao(
+     categoria,
+     pergunta,
+     resposta
+     )
+    
+
