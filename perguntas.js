f36a2128805a41190d429e7d0cc483826f360785
Maria Clara <132940309+santiagoclara@users.noreply.github.com>
2024-11-26 09:08:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+'Mundo Pop',
+'Quem é a melhor cantora do mundo?',
+'Beyoncé Giselle Knowles-Carter'
+)
+criaCartao(
+    'Beyoncé',
+    'Qual seu melhor álbum?',
+    'O álbum auto-denominado, Beyoncé'
+    )
+ criaCartao(
+     "Opnião da autora",
+     "Quais as melhores músicas da Beyoncé segundo a criadora do site?",
+     "Formation, Love Drought and XO."
+     )
+   
